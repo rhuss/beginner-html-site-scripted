8a9eab765f1d286b0b649f9f577f8bf5503d99c2
Roland Huß <roland@ro14nd.de>
2017-11-21 14:56:23 +0100

    Update main.js

--- scripts/main.js
+++ scripts/main.js
@@ -26,7 +26,7 @@ if(!localStorage.getItem('name')) {
   setUserName();
 } else {
   var storedName = localStorage.getItem('name');
-  myHeading.innerHTML = 'Mozilla is cool, ' + storedName;
+  myHeading.innerHTML = 'DevOpsCon is cool, ' + storedName;
 }
 
 myButton.onclick = function() {
